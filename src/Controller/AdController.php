2359e2286953db7478307f1f816725148373d244
Boderio <berti@epse.be>
2025-10-15 09:59:19 +0200

    cours 4 - insertion dans la bdd

--- src/Controller/AdController.php
+++ src/Controller/AdController.php
@@ -6,8 +6,10 @@ use App\Entity\Ad;
 use App\Form\AnnonceType;
 use App\Repository\AdRepository;
 //use Doctrine\Persistence\ManagerRegistry;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bridge\Doctrine\Attribute\MapEntity;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -31,11 +33,24 @@ final class AdController extends AbstractController
     }
 
     #[Route('/ads/new', name: 'ads_create')]
-    public function create(): Response
+    public function create(Request $request, EntityManagerInterface $manager): Response
     {
         $ad = new Ad();
         $form = $this->createForm(AnnonceType::class,$ad);
+        // permet de vérifier l'état du formulaire (envoyé ou non par exemple)
+        $form->handleRequest($request);
+        //dump($request);
+        // isSubmitted : permet de vérifier si formulaire soumis ou non?
+        // isValid : permet de vérifier si le formulaire est valide ou non
+        //$arrayForm = $request->request->all();
 
+        if($form->isSubmitted() && $form->isValid())
+        {
+            //dump($arrayForm['annonce']);
+            $manager->persist($ad);
+            $manager->flush();
+            return $this->redirectToRoute('ads_show',['slug'=>$ad->getSlug()]);
+        }
 
         return $this->render('ad/new.html.twig',[
             'myForm' => $form->createView()
